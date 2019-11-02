64695a3e5dba433f22bb237fd77d2d0f47db514a
DARIUS <grigdarius@gmail.com>
2019-11-02 19:21:26 +0100

    can send which state to blink, some testing code

--- src/our_ur/scripts/RSD_GUI.py
+++ src/our_ur/scripts/RSD_GUI.py
@@ -1,5 +1,6 @@
 import sys
 import time
+import threading
 
 from PySide2.QtWidgets import QApplication, QMainWindow
 from PySide2.QtCore import *
@@ -61,7 +62,53 @@ class Packml_GUI(QThread):
 
     @Slot(str)
     def Error_message(self, message):
+        if message != "No errors":
+            self.BoolErrorMessage = True
+        else:
+            self.BoolErrorMessage = False
         self.main_window.labelErrormessage.setText(message)
+
+
+    @Slot(str)
+    def CurrentState(self, state):
+        if state == "ABORTED":
+            self.CurrentStateIndex = 1
+        elif state == "ABORTING":
+            self.CurrentStateIndex = 2
+        elif state == "CLEARING":
+            self.CurrentStateIndex = 3
+        elif state == "COMPLETE":
+            self.CurrentStateIndex = 4
+        elif state == "COMPLETING":
+            self.CurrentStateIndex = 5
+        elif state == "EXECUTING":
+            self.CurrentStateIndex = 6
+        elif state == "HELD":
+            self.CurrentStateIndex = 7
+        elif state == "HOLDING":
+            self.CurrentStateIndex = 8
+        elif state == "IDLE":
+            self.CurrentStateIndex = 9
+        elif state == "RESETING":
+            self.CurrentStateIndex = 10
+        elif state == "STARTING":
+            self.CurrentStateIndex = 11
+        elif state == "STOPPED":
+            self.CurrentStateIndex = 12
+        elif state == "STOPPING":
+            self.CurrentStateIndex = 13
+        elif state == "SUSPENDED":
+            self.CurrentStateIndex = 14
+        elif state == "SUSPENDING":
+            self.CurrentStateIndex = 15
+        elif state == "UNHOLDING":
+            self.CurrentStateIndex = 16
+        elif state == "UNSUSPENDING":
+            self.CurrentStateIndex = 17
+        else:
+            print ("WRONG COMMAND SENT TO CURRENT STATE UPDATEs")
+        self.disable_all_frames()
+        # self.enable_frame()
 
     def __init__(self):
         QThread.__init__(self)
@@ -71,6 +118,9 @@ class Packml_GUI(QThread):
         loader = QUiLoader()
         self.main_window = loader.load(ui_file)
         # self.main_window.
+        self.disable_all_frames()
+        self.CurrentStateIndex = 9
+        self.BoolErrorMessage = False
         self.main_window.labelOEEValue.setText("0")
         self.main_window.labelUptimeValue.setText("0")
         self.main_window.labelErrormessage.setText("No errors")
@@ -115,17 +165,43 @@ class Packml_GUI(QThread):
     
     def run(self):
         while True:
-            self.disable_all_frames()
+            
             self.msleep(500)
-            for key in IndexToFrame:
-                self.enable_frame(key)
+            self.enable_frame(self.CurrentStateIndex)
+            if self.BoolErrorMessage == True:
+                self.main_window.labelErrormessage.setStyleSheet("background-color: rgb(255, 0, 0);")
             self.msleep(500)
-    
+            self.disable_frame(self.CurrentStateIndex)
+            if self.main_window.labelErrormessage.styleSheet() == "background-color: rgb(255, 0, 0);" or self.BoolErrorMessage == False:
+                self.main_window.labelErrormessage.setStyleSheet("")
 
+#THIS CLASS ONLY FOR TESTING
+class Communicate(QObject):                                                 
+ # create a new signal on the fly and name it 'speak'                       
+ speak = Signal(str)    
+
+def test_func():
+    print ("Making connection")
+    someone = Communicate()                                                     
+    # connect signal and slot                                                   
+    someone.speak.connect(GUI.CurrentState)
+    someone.speak.connect(GUI.Error_message)
+    while True:
+        print ("Next state: ")
+        cmd = input("> ")
+        # print ("Transitioning to state: {}".format(cmd))
+        someone.speak.emit(cmd)
 
 if __name__ == "__main__":
+    QCoreApplication.setAttribute(Qt.AA_ShareOpenGLContexts)
     app = QApplication(sys.argv)
 
     GUI = Packml_GUI()
     GUI.start()
+    inputThread = threading.Thread(target=test_func, args=())
+    inputThread.daemon = True
+    inputThread.start()
     sys.exit(app.exec_())
+    
+    
+    
