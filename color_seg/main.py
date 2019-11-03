4e4b643fc2ee36b6f17b0fdaa922aaacf1894d81
mohamed <moh.abouseif@gmail.com>
2019-11-03 21:29:53 +0100

    Modified main.py for better perfromance and comprehension

--- color_seg/main.py
+++ color_seg/main.py
@@ -1,30 +1,21 @@
 import cv2
 import numpy as np
-
+from collections import deque
 
 
 # # Red params
-low_red = [160, 120, 120]  
+low_red = [179, 120, 0]  
 high_red = [180, 255, 255]
 
 # # Yellow params
-low_yellow = [10, 70, 170] # 20 120 0
-high_yellow = [30, 200, 255] # 35 255 255
+low_yellow = [10, 120, 0] # 20 120 0
+high_yellow = [35, 255, 255] # 35 255 255
 
 # # Blue params
-low_blue = [100, 120, 120]
+low_blue = [100, 120, 0]
 high_blue = [140, 255, 255]
 
-def process_img(src, color_name, low, high, k_erosion=3, k_dilation=11, erosion_iterations=5, dilation_iterations=5):
-
-    if color_name == "red":
-        color_code = (0, 0, 255)
-    if color_name == "yellow":
-        color_code = (0, 255, 255)
-    if color_name == "blue":
-        color_code = (255, 0, 0)
-    
-    drawing = np.zeros_like(src)
+def process_img_binary(src, low, high, k_erosion=3, k_dilation=5, erosion_iterations=5, dilation_iterations=2):
 
     filter_size = 29
     src = cv2.GaussianBlur(src, (filter_size, filter_size), cv2.BORDER_DEFAULT)
@@ -36,47 +27,135 @@ def process_img(src, color_name, low, high, k_erosion=3, k_dilation=11, erosion_
     src = cv2.erode(src, str_el_erosion, iterations=erosion_iterations)
     binary = src
 
+    return binary
+
+
+def process_img_contours(binary_img, color_name):
+
+    if color_name == "red":
+        color_code = (0, 0, 255)
+        min_threshold_area = 2500
+        max_threshold_area = 3500
+    if color_name == "yellow":
+        color_code = (0, 255, 255)
+        min_threshold_area = 6500
+        max_threshold_area = 7500
+    if color_name == "blue":
+        color_code = (255, 0, 0)
+        min_threshold_area = 1100
+        max_threshold_area = 1800
+    
+    drawing = np.zeros(binary_img.shape + (3,))
+
+    src = np.uint8(binary_img)
     thresh = 50
     canny = cv2.Canny( src, thresh, thresh*2, 3 )
     contours, hierarchy = cv2.findContours( canny, cv2.CHAIN_APPROX_SIMPLE, cv2.RETR_TREE)[-2:]
 
     minRect = [cv2.minAreaRect(contour) for contour in contours]
-
-    for i in range(len(contours)):
-        cv2.drawContours(drawing, contours, i, color_code, 2, 8, hierarchy, ) 
+    
+    # for i in range(len(contours)):
+    #     # Should the -1 be i or not?
+    #     cv2.drawContours(drawing, contours, i, color_code, 2, 8)# , hierarchy[:][0]) 
         
-        box = cv2.boxPoints(minRect[i])
-        box = np.int0(box)
-        for j in range(4):
-            cv2.line( drawing, tuple(box[j]), tuple(box[(j+1)%4]), (128,0,128), 3, 8)
-        # cv2.drawContours(drawing,[box],0,(0,255,255),2)
-        cv2.putText(drawing, color_name, tuple(box[1]), cv2.FONT_HERSHEY_SIMPLEX, 1, color_code)
+    #     box = cv2.boxPoints(minRect[i])
+    #     box = np.int0(box)
+    #     for j in range(4):
+    #         cv2.line( drawing, tuple(box[j]), tuple(box[(j+1)%4]), (128,0,128), 3, 8)
+    #     cv2.putText(drawing, color_name, tuple(box[1]), cv2.FONT_HERSHEY_SIMPLEX, 1, color_code)
 
-    return binary, drawing
+    
+    
+    if (len(contours) > 0):
+        area = cv2.contourArea(contours[0])
+        # print(area)
+        if (color_name == "red" and area > min_threshold_area and area < max_threshold_area):
+            cv2.drawContours(drawing, contours[0], -1, color_code, 2, 8)#  , hierarchy[0][0]) 
+            box = cv2.boxPoints(minRect[0])
+            box = np.int0(box)
+            for j in range(4):
+                cv2.line( drawing, tuple(box[j]), tuple(box[(j+1)%4]), (128,0,128), 3, 8)
+            cv2.putText(drawing, color_name, tuple(box[1]), cv2.FONT_HERSHEY_SIMPLEX, 1, color_code)
+            print("{} brick deteced!".format(color_name))
+
+        if (color_name == "yellow" and area > min_threshold_area and area < max_threshold_area):
+            cv2.drawContours(drawing, contours[0], -1, color_code, 2, 8)#  , hierarchy[0][0]) 
+            box = cv2.boxPoints(minRect[0])
+            box = np.int0(box)
+            for j in range(4):
+                cv2.line( drawing, tuple(box[j]), tuple(box[(j+1)%4]), (128,0,128), 3, 8)
+            cv2.putText(drawing, color_name, tuple(box[1]), cv2.FONT_HERSHEY_SIMPLEX, 1, color_code)
+            print("{} brick deteced!".format(color_name))
+
+        if (color_name == "blue" and area > min_threshold_area and area < max_threshold_area):
+            cv2.drawContours(drawing, contours[0], -1, color_code, 2, 8)#  , hierarchy[0][0]) 
+            box = cv2.boxPoints(minRect[0])
+            box = np.int0(box)
+            for j in range(4):
+                cv2.line( drawing, tuple(box[j]), tuple(box[(j+1)%4]), (128,0,128), 3, 8)
+            cv2.putText(drawing, color_name, tuple(box[1]), cv2.FONT_HERSHEY_SIMPLEX, 1, color_code)
+            print("{} brick deteced!".format(color_name))
+
+
+    return drawing
 
 if __name__ == "__main__":
-    img = cv2.imread("./lego_images/4.jpg")
-    img = img[1000:2000, 700:2500]
-    # binary, dst = process_img(img, "red", low_red, high_red)
-    # binary, dst = process_img(img, "yellow", low_yellow, high_yellow)
-    binary, dst = process_img(img, "blue", low_blue, high_blue)
 
-    cv2.namedWindow('image',cv2.WINDOW_NORMAL)
-    cv2.resizeWindow('image', 400,400)
-    cv2.moveWindow ('image', 0, 0)
-    cv2.imshow('image', img)
+    cap = cv2.VideoCapture("http://10.42.0.96:8000/stream.mjpg")
+    if not cap:
+        print("Nothing captured")
 
-    cv2.namedWindow('binary',cv2.WINDOW_NORMAL)
-    cv2.resizeWindow('binary', 400,400)
-    cv2.moveWindow ('binary', 0, 400)
-    cv2.imshow('binary', binary)
-
-    cv2.namedWindow('processed',cv2.WINDOW_NORMAL)
-    cv2.resizeWindow('processed', 800, 800)
-    cv2.moveWindow ('processed', 450, 0)
-    cv2.imshow('processed', dst)
+    red_deque = deque(maxlen=10)
+    yellow_deque = deque(maxlen=10)
+    blue_deque = deque(maxlen=10)
     
+    while(True):
+        red_count = 0
+        yellow_count = 0
+        blue_count = 0
 
-    key = cv2.waitKey(30)
-    while(key != 27 or key != "q"):
-        pass
+        ret, frame = cap.read()
+        frame = frame[200:400, 150:450]
+        drawing = np.zeros_like(frame)
+
+        binary_red = process_img_binary(frame, low_red, high_red)
+        # binary_red = cv2.bitwise_not(binary_red)
+        binary_yellow = process_img_binary(frame, low_yellow, high_yellow)
+        binary_blue = process_img_binary(frame, low_blue, high_blue)
+        
+        red_deque.append(binary_red)
+        yellow_deque.append(binary_yellow)
+        blue_deque.append(binary_blue)
+        binary_red = np.average(red_deque, axis=0)
+        binary_yellow = np.average(yellow_deque, axis=0)
+        binary_blue = np.average(blue_deque, axis=0)
+
+        dst_red = process_img_contours(binary_red, "red")
+        dst_yellow = process_img_contours(binary_yellow, "yellow")
+        dst_blue = process_img_contours(binary_blue, "blue")
+
+        binary_all = binary_red + binary_yellow + binary_blue
+        dst_all = dst_red + dst_yellow + dst_blue
+
+
+        cv2.namedWindow('Feed',cv2.WINDOW_NORMAL)
+        cv2.resizeWindow('Feed', 400,400)
+        cv2.moveWindow ('Feed', 0, 0)
+        cv2.imshow('Feed', frame)
+
+        cv2.namedWindow('binary',cv2.WINDOW_NORMAL)
+        cv2.resizeWindow('binary', 400,400)
+        cv2.moveWindow ('binary', 0, 400)
+        cv2.imshow('binary', binary_all)
+
+        cv2.namedWindow('processed',cv2.WINDOW_NORMAL)
+        cv2.resizeWindow('processed', 800, 800)
+        cv2.moveWindow ('processed', 450, 0)
+        cv2.imshow('processed', dst_all)
+
+        key = cv2.waitKey(1)
+        while(key & 0xFF == ord('q')):
+            break
+    
+    cap.release()
+    cv2.destroyAllWindows()
